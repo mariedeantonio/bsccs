1e21ebf99905c5fb44adec6aec35a846bdc0a40f
cokibeach@gmail.com <cokibeach@gmail.com@f7c91f03-731f-d806-b965-e12744d8cf89>
2011-01-21 23:19:09 +0000

    Creating branch for Coki's work

---
+++ branches/coki-branch/C/codebase/CCD/AbstractSelector.cpp
@@ -0,0 +1,41 @@
+/*
+ * AbstractSelector.cpp
+ *
+ *  Created on: Nov 17, 2010
+ *      Author: msuchard
+ */
+
+#include <iostream>
+#include <algorithm>
+
+#include "AbstractSelector.h"
+
+AbstractSelector::AbstractSelector(
+		std::vector<int>* inIds,
+		SelectorType inType,
+		long inSeed) : ids(inIds), type(inType), seed(inSeed), K(inIds->size()) {
+
+	// Set up number of exchangeable objects
+	if (type == SUBJECT) {
+		N = *(std::max_element(ids->begin(), ids->end())) + 1;
+	} else {
+		N = ids->size();
+	}
+
+	// Set up seed
+	if (seed == -1) {
+		deterministic = true;
+	} else {
+		deterministic = false;
+		if (seed == 0) {
+			seed = time(NULL);
+		}
+		srand(seed);
+	}
+}
+
+AbstractSelector::~AbstractSelector() {
+	if (ids) {
+		delete ids;
+	}
+}
